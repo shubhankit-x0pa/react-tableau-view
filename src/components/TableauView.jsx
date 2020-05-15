49d508acdc6fe660010149d2f17751549b981ec3
Birendra Kumar <birendra12sep@gmail.com>
2020-05-15 18:32:43 +0530

    ggitignore file updated

--- src/components/TableauView.jsx
+++ src/components/TableauView.jsx
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 
+import TableauAPI from '../vendors';
+import Utils from '../utils';
+
 const TableauView = (props) => {
+  const tableauVizEl = useRef(null);
   const {
     className,
+    url,
+    ticket,
+    queryParams,
+    options,
+    filters,
+    parameters,
   } = props;
 
   return (
-    <div className={className}>
+    <div className={`Tableau-view-container ${className}`}>
       Tableau View
+      Welcome to New Library
+      <div ref={inputEl} />
     </div>
   );
 }
 
 TableauView.propTypes = {
+  url: PropTypes.string.isRequired,
+  ticket: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
   className: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.object,
   ]),
-
+  queryParams: PropTypes.string,
+  options: PropTypes.object,
+  filters: PropTypes.object,
+  parameters: PropTypes.object,
 };
 
 export default TableauView;
