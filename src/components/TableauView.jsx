cf31f50a55c38dea4f59deaf62105e8a5effa160
Birendra Kumar <birendra12sep@gmail.com>
2020-05-17 15:19:16 +0530

    test

--- src/components/TableauView.jsx
+++ src/components/TableauView.jsx
@@ -5,101 +5,87 @@ import URL from 'url';
 
 import Utils from '../utils';
 
-class TableauView extends Component {
+const TableauView = () => {
   
-  constructor(props) {
-    super(props);
-    this.tableauVizEl = createRef();
-    this.state = {
-      viz: null,
-    };
-  }
   
-  // const [tableauViz, setTableauViz] = useState(null);
-  // const {
-  //   className,
-  //   url,
-  //   ticket,
-  //   queryParams = '?:toolbar=yes&:embed=yes&:refresh=yes&:comments=no',
-  //   options,
-  //   filters,
-  //   parameters,
-  // } = props;
+  const [tableauViz, setTableauViz] = useState(null);
+  const {
+    className,
+    url,
+    ticket,
+    queryParams = '?:toolbar=yes&:embed=yes&:refresh=yes&:comments=no',
+    options,
+    filters,
+    parameters,
+  } = props;
 
-  componentDidMount() {
-    // Calling the function after the page has loaded
-    this.initViz();
-  }
+  
+  // /**
+  //  * Get the tableau viz url. If ticket given then generate trusted ticket tableau viz url 
+  //  * @param {void}
+  //  * @return {string} URL The tableau viz url
+  //  */
+  // getTableauVizURL() {
+  //   const {url, ticket, queryParams} = this.props;
+  //   if (!url) {
+  //     throw new Error('Error: Please give the valid url');
+  //   }
+  //   const parsed = URL.parse(url, true);
 
-  /**
-   * Get the tableau viz url. If ticket given then generate trusted ticket tableau viz url 
-   * @param {void}
-   * @return {string} URL The tableau viz url
-   */
-  getTableauVizURL() {
-    const {url, ticket, queryParams} = this.props;
-    if (!url) {
-      throw new Error('Error: Please give the valid url');
-    }
-    const parsed = URL.parse(url, true);
+  //   if (ticket) {
+  //     return `${Utils.constructTrustedTicketURL(url, ticket)}${queryParams}`;
+  //   }
 
-    if (ticket) {
-      return `${Utils.constructTrustedTicketURL(url, ticket)}${queryParams}`;
-    }
+  //   const {protocol, host, pathname} = parsed;
+  //   return `${protocol}//${host}${pathname}${queryParams}`;
+  // }
 
-    const {protocol, host, pathname} = parsed;
-    return `${protocol}//${host}${pathname}${queryParams}`;
-  }
+  // /**
+  //  * Initialize the tableau js viz api to display the visualization.
+  //  * @param {void}
+  //  * @return {void}
+  //  */
+  // initViz() {
+  //   const {filters, parameters, options: propOptions} = this.props;
+  //   const {viz} = this.state;
 
-  /**
-   * Initialize the tableau js viz api to display the visualization.
-   * @param {void}
-   * @return {void}
-   */
-  initViz() {
-    const {filters, parameters, options: propOptions} = this.props;
-    const {viz} = this.state;
+  //   const tableauVizUrl = this.getTableauVizURL();
 
-    const tableauVizUrl = this.getTableauVizURL();
+  //   const options = {
+  //     ...filters,
+  //     ...parameters,
+  //     ...propOptions,
+  //     onFirstInteractive: () => {
+  //       if (viz) {
+  //         let sheet = viz.getWorkbook().getActiveSheet();
 
-    const options = {
-      ...filters,
-      ...parameters,
-      ...propOptions,
-      onFirstInteractive: () => {
-        if (viz) {
-          let sheet = viz.getWorkbook().getActiveSheet();
+  //         // Check child sheets exist or not.
+  //         if (typeof sheet.getWorksheets !== 'undefined') {
+  //           const childSheets = sheet.getWorksheets();
 
-          // Check child sheets exist or not.
-          if (typeof sheet.getWorksheets !== 'undefined') {
-            const childSheets = sheet.getWorksheets();
+  //           if (childSheets && childSheets.length) {
+  //             sheet = childSheets[0];
+  //           }
+  //         }
+  //       }
+  //     }
+  //   };
 
-            if (childSheets && childSheets.length) {
-              sheet = childSheets[0];
-            }
-          }
-        }
-      }
-    };
+  //   // to clean the previous viz
+  //   if (viz) {
+  //     viz.dispose();
+  //     this.setState({viz: null});
+  //   }
 
-    // to clean the previous viz
-    if (viz) {
-      viz.dispose();
-      this.setState({viz: null});
-    }
+  //   let updatedViz = new tableauSoftware.Viz(this.tableauVizEl, tableauVizUrl, options);
+  //   this.setState({ viz: updatedViz });
+  // };
 
-    let updatedViz = new tableauSoftware.Viz(this.tableauVizEl, tableauVizUrl, options);
-    this.setState({ viz: updatedViz });
-  };
-
-  render() {
-    const { className} = this.props;
     return (
       <div className={`Tableau-view-container ${className}`}>
-        <div ref={this.tableauVizEl} />
+        {/* <div ref={this.tableauVizEl} /> */}
       </div>
     );
-  }
 }
 
 TableauView.propTypes = {
